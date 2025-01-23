cfc28c95caab49e62c5418dfc4de35e0fa435667
chiranjibkonar <chiranjib.konar@gmail.com>
2025-01-23 15:00:11 +0530

    Update RadioLum-n-Beq.py

--- RadioLum-n-Beq.py
+++ RadioLum-n-Beq.py
@@ -5,9 +5,7 @@ C_3 = 2.368e-03   # in CGS unit
 pi=3.14159 
 me=9.1093837e-28    #CGS 
 c=2.99792458e+10    #CGS
-#file upload and read
 
-#  #####
 def E(gamma):
   E = (gamma*me*c**2) 
   return E
@@ -25,5 +23,12 @@ def Beq(V,alpha,z,D_LMpc,nuo_MHz, So_nuo_Jy,gamma_1, gamma_2):
   Term_5=E_2**(1-2*alpha) - E_1**(1-2*alpha)
   Beq= (Term_1*Term_2*Term_3*Term_4*Term_5)**(1/(3+alpha))   
   return Beq 
-st.write(Beq(1e70, 0.75, 0.02, 100, 600, 10, 10, 1e+5))    
+#st.write(Beq(1e70, 0.75, 0.02, 100, 600, 10, 10, 1e+5))    
+# Taking inputs
+inputs=st.text_input("Give V, alpha, z, D_LMpc, nuo_MHz, So_nuo_Jy, gamma_1, gamma_2")  # it will create a slot in streamlit for taking inouts 
+values=Inputs.split(",") 
+V, alpha, z, D_LMpc, nuo_MHz, So_nuo_Jy, gamma_1, gamma_2 = map(float, values) 
+#  #####
+st.write(Beq(V, alpha, z, D_LMpc, nuo_MHz, So_nuo_Jy, gamma_1, gamma_2))
+
 st.write("output") 
